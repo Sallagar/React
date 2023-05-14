85e4b0ff74aef2a8000d16bc162daed4758afb7a
Ivan <ivankononenko772@gmail.com>
2023-05-14 11:24:16 +0300

    add selector

--- src/components/users/UsersContainer.jsx
+++ src/components/users/UsersContainer.jsx
@@ -5,23 +5,32 @@ import {
     unfollow,
     setCurrentPage,
     toggleFollowingProgres,
-    getUsers,
+    requestUsers,
 } from './../../redux/users-reducer'
 import Users from './Users'
 import Preloader from './../common/preloader/preloader'
 import { Navigate } from "react-router-dom"
 import { withAuthRedirect } from './../../hoc/withAuthRedirect';
 import { compose } from 'redux'
+import { 
+    getUsers, 
+    getCurrentPage, 
+    getFollowingInProgres, 
+    getIsAuth, 
+    getIsFetching, 
+    getPageSize, 
+    getTotalUsersCount 
+} from '../../redux/users-selectors'
 
 
 
 class UsersContainer extends React.Component {
     componentDidMount () {
-        this.props.getUsers(this.props.currentPage, this.props.pageSize)
+        this.props.requestUsers(this.props.currentPage, this.props.pageSize)
     }
     
     onPageChanged = (pageNumber) => {
-        this.props.getUsers(pageNumber, this.props.pageSize)
+        this.props.requestUsers(pageNumber, this.props.pageSize)
         
         
     }
@@ -45,15 +54,27 @@ class UsersContainer extends React.Component {
     }
 }
 
+// const mapStateToProps = (state) => {
+//     return{
+//         users: state.usersPage.users,
+//         pageSize: state.usersPage.pageSize,
+//         totalUsersCount: state.usersPage.totalUsersCount,
+//         currentPage: state.usersPage.currentPage,
+//         isFetching: state.usersPage.isFetching,
+//         followingInProgres: state.usersPage.followingInProgres,
+//         isAuth: state.auth.isAuth
+//     }
+// }
+
 const mapStateToProps = (state) => {
     return{
-        users: state.usersPage.users,
-        pageSize: state.usersPage.pageSize,
-        totalUsersCount: state.usersPage.totalUsersCount,
-        currentPage: state.usersPage.currentPage,
-        isFetching: state.usersPage.isFetching,
-        followingInProgres: state.usersPage.followingInProgres,
-        isAuth: state.auth.isAuth
+        users: getUsers(state),
+        pageSize: getPageSize(state),
+        totalUsersCount: getTotalUsersCount(state),
+        currentPage: getCurrentPage(state),
+        isFetching: getIsFetching(state),
+        followingInProgres: getFollowingInProgres(state),
+        isAuth: getIsAuth(state)
     }
 }
 
@@ -63,7 +84,7 @@ export default compose(
         unfollow,
         setCurrentPage,
         toggleFollowingProgres,
-        getUsers,
+        requestUsers,
     }),
     withAuthRedirect
 )(UsersContainer)
