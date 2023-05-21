4acef4740c3c273459a160190a6572149d0173bf
Ivan <ivankononenko772@gmail.com>
2023-05-21 13:27:36 +0300

    addUseStateToProfile

--- src/components/profile/myPosts/ProfileInfo/ProfileInfo.jsx
+++ src/components/profile/myPosts/ProfileInfo/ProfileInfo.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Preloader from "../../../common/preloader/preloader";
 import profInf from './ProfileInfo.module.css'
-import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHooks from './ProfileStatusWithHooks'
 
 const ProfileInfo = (props) => {
   if (!props.profile) {
@@ -16,7 +16,7 @@ const ProfileInfo = (props) => {
         alt='img'
          />
         <div>{props.profile.fullName}</div>
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         <div>{props.profile.contacts.github}</div>
       </div>
     </div>
